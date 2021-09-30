803f9ba7e03f3495a7453552f59c5f1004ba0788
treserio <treserio@hotmail.com>
2021-09-29 23:51:12 -0500

    1-5 init

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("{:d} argument:".format(1))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+
+    for i in range(1, (len(sys.argv))):
+        print("{:d}: {}".format(i, sys.argv[i]))
