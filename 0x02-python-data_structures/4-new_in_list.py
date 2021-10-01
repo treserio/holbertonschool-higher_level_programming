1a9de871ea48364ea82190ab054cb2158816311f
treserio <treserio@hotmail.com>
2021-09-30 21:47:59 -0500

    4.1 slight restyle

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if not my_list:
-        return None
     new = my_list[:]
     if idx > len(my_list) - 1 or idx < 0:
         return my_list
