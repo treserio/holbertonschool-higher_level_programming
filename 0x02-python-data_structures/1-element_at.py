cc2683f85b1969b9ed05c3a90b5fc23a80db232b
treserio <treserio@hotmail.com>
2021-09-30 22:10:06 -0500

    1.1 refactored

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    for i in range(len(my_list)):
-        if i == idx:
-            return my_list[i]
+    if idx < len(my_list) and idx > 0:
+        return my_list[idx]
