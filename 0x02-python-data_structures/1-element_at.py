a69c6148027277053ebba97749f43e3f4d401af1
treserio <treserio@hotmail.com>
2021-09-30 21:34:15 -0500

    1-6 init

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    for x in range(len(my_list)):
-        if x == idx:
-            return my_list[x]
+    for i in range(len(my_list)):
+        if i == idx:
+            return my_list[i]
