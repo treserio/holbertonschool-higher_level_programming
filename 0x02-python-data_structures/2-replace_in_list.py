c603e82e4aa08c0227091f845f40c6d81c002a0d
treserio <treserio@hotmail.com>
2021-09-30 22:33:45 -0500

    2.1 refactored

--- 0x02-python-data_structures/2-replace_in_list.py
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for i in range(len(my_list)):
-        if i == idx:
-            my_list[i] = element
-            break
+    if idx < len(my_list) and idx >= 0:
+        my_list[idx] = element
     return my_list
