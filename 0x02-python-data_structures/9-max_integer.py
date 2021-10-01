170e44d7783a2473b3e158e845ac878d57328aba
treserio <treserio@hotmail.com>
2021-10-01 01:16:26 -0500

    9.1 refactor

--- 0x02-python-data_structures/9-max_integer.py
+++ 0x02-python-data_structures/9-max_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        largest = my_list[0]
-        for x in my_list:
-            if(x > largest):
-                largest = x
-        return largest
+        return sorted(my_list)[-1]
