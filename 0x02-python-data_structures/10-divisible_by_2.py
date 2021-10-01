ebc1bde7274ac916bc57b20ac57ae8c2f584a456
treserio <treserio@hotmail.com>
2021-09-30 22:47:27 -0500

    10.1 peped

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -3,5 +3,5 @@ def divisible_by_2(my_list=[]):
     if my_list:
         thing = []
         for x in my_list:
-            thing.append(False if x%2 else True)
+            thing.append(False if x % 2 else True)
         return thing
