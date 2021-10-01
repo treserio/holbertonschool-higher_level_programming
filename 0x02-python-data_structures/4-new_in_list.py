e289f2eba80b5caea14b1c9c1f9353fb1a7c777c
treserio <treserio@hotmail.com>
2021-09-30 23:00:36 -0500

    4.2 refactor

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new = my_list[:]
-    if idx > len(my_list) - 1 or idx < 0:
+    if idx < len(my_list) and idx >= 0:
+        thing = my_list[:]
+        thing[idx] = element
+        return thing
+    else:
         return my_list
-    new[idx] = element
-    return new
