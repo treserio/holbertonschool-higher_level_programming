a9438b7bf53cdcbd938908769c663f317e0d82c5
treserio <treserio@hotmail.com>
2021-10-01 01:37:01 -0500

    10.2 refactored

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list:
-        thing = []
-        for x in my_list:
-            thing.append(False if x % 2 else True)
-        return thing
+        return [False if x % 2 else True for x in my_list if my_list]
