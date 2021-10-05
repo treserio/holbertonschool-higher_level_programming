c341896fb66122e2e450158c884c19f9fae37ea2
treserio <treserio@hotmail.com>
2021-10-05 01:49:08 -0500

    12.5 added str check

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     vals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
-    if roman_string:
+    if type(roman_string) == str:
         j = iter(list(roman_string[1:]))
         for i in roman_string:
             k = next(j, i)
