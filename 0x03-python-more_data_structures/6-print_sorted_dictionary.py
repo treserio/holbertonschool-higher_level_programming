8fd1fa8ecb527fba279cce0012de6e82797a1be8
treserio <treserio@hotmail.com>
2021-10-05 01:38:58 -0500

    6.1 consider empty str

--- 0x03-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dic):
-    print("\n".join("{}: {}".format(x, a_dic[x]) for x in sorted(a_dic)))
+def print_sorted_dictionary(dic):
+    print("\n".join("{}: {}".format(x, dic[x]) for x in sorted(dic) if dic))
