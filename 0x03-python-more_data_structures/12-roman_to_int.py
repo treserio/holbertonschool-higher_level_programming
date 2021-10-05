540c6f427688d68161fb54e9c2b17dc8bc5def3c
treserio <treserio@hotmail.com>
2021-10-05 00:58:00 -0500

    1, 2 init

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     vals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    total = int(0)
+    total = 0
     if roman_string:
         j = iter(list(roman_string[1:]))
         for i in roman_string:
