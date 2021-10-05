5bfe9fcaa7334eedbc2c33bf385e0056f8298619
treserio <treserio@hotmail.com>
2021-10-05 01:44:51 -0500

    6.2 fixed for empty

--- 0x03-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(dic):
-    print("\n".join("{}: {}".format(x, dic[x]) for x in sorted(dic) if dic))
+    if dic:
+        print("\n".join("{}: {}".format(x, dic[x]) for x in sorted(dic)))
