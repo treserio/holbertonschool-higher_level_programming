739793a21418d408e4f1c80e7a09c6e8f90625e2
treserio <treserio@hotmail.com>
2021-10-07 22:25:48 -0500

    8.1 +.format for chkr

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print("".join(chr(ord(l)-32) if l >= 'a' and l <= 'z' else l for l in str))
+    print("".join("{:c}".format(ord(l)-32) if l >= 'a' and l <= 'z'
+          else l for l in str))
