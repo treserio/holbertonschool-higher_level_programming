12a79e11b7ff9a83f11d5b0a852a07452ffcad59
treserio <treserio@hotmail.com>
2021-10-11 23:11:35 -0500

    4 init

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+'''Prints a square of size dimensions
+'''
+
+
+def print_square(size):
+    '''Prints a square of size dimensions
+
+    Args:
+        size: dimensions of the square to print
+    '''
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size:
+        print("\n".join(("#"*size) for x in range(size)))
