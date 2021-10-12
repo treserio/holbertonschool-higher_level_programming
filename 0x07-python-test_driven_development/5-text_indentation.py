e75acbf6d2149447e280dac3302fe722f31551a5
treserio <treserio@hotmail.com>
2021-10-12 00:19:33 -0500

    5.1 refactored

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-"""This module prints 2 newlines after . ? and :"""
+'''Print input str with 2 \\n\\n after . ? and :
+'''
 
 
 def text_indentation(text):
-    """Prints newlines after . ? and :"""
-
-    if type(text) is not str:
-        raise TypeError("text must be a string")
-
-    for i in text:
-        if i == ' ' and flag == 1:
-            continue
-
-        if i == '.' or i == '?' or i == ':':
-            print("{}{}".format(i, '\n'))
-            flag = 1
-
-        else:
-            print(i, end="")
-            flag = 0
+    '''Print input str with 2 \\n\\n after . ? and :
+    '''
+    print(text.replace(". ", ".\n\n").replace("? ", "?\n\n")
+          .replace(": ", ":\n\n"), end="")
