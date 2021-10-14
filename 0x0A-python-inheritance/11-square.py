5fb8b0c6d474e3ccf6ec48e5accd31acf3d0b24f
treserio <treserio@hotmail.com>
2021-10-13 20:39:00 -0500

    11 init

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Validator version of Class BaseGeometry'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Rectangle class inheriting from'''
+
+    def __init__(self, size):
+        '''Square init method that also sets height and width'''
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
