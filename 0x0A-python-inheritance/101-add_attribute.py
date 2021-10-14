1a001b6862047c88b2afba18d07b03dd68db7f2a
treserio <treserio@hotmail.com>
2021-10-13 22:04:22 -0500

    101.1 chk for __dict__ in obj

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def add_attribute(obj, key, val):
-    if type(obj) not in [int, float, tuple, str, list, dict]:
-        if not hasattr(obj, key):
-            setattr(obj, key, val)
+    if hasattr(obj, '__dict__'):
+        setattr(obj, key, val)
     else:
         raise TypeError("can't add new attribute")
