c3874219dc514720848320f79258da6f9cca7509
treserio <treserio@hotmail.com>
2021-10-13 22:06:34 -0500

    101.2 peped

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+'''Add attribute to obj if able'''
+
+
 def add_attribute(obj, key, val):
+    '''Add attribute to obj if able'''
     if hasattr(obj, '__dict__'):
         setattr(obj, key, val)
     else:
