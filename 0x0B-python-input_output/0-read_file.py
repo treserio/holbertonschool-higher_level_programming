75cb82afed7cc91e5c6fca15d10c0b5111880e62
treserio <treserio@hotmail.com>
2021-10-15 18:10:10 -0500

    0 init

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Read a file and print to stdout'''
+
+
+def read_file(filename=""):
+    '''Read a file and print to stdout'''
+    with open(filename, 'r') as fd:
+        print(fd.read())
