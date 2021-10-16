54a4f5849804d0434cc81d4d16f0c30a98099441
treserio <treserio@hotmail.com>
2021-10-15 22:36:19 -0500

    11 init

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''Module for Student class'''
+
+
+class Student:
+    '''Student class'''
+
+    def __init__(self, first_name, last_name, age):
+        '''Creates a Student
+
+        Args:
+            first_name: the student's first name
+            last_name: the student's last name
+            age: the student's age
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Returns dictionary representation of obj
+
+        Args:
+            attrs: list of keys to JSONify
+        '''
+        if attrs is not None:
+            return {k: self.__dict__[k] for k in self.__dict__ if k in attrs}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for k in json:
+            setattr(self, k, json[k])
