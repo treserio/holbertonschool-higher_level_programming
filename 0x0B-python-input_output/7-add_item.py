42d3a9898128bae93ab25c5008e817544417f4e8
treserio <treserio@hotmail.com>
2021-10-15 20:34:49 -0500

    7.1 try load first

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 '''Save all args from list to file'''
-import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file(sys.argv[1:], 'add_item.json')
+try:
+    out = load_from_json_file('add_item.json')
+except:
+    out = []
+
+save_to_json_file(out + sys.argv[1:], 'add_item.json')
