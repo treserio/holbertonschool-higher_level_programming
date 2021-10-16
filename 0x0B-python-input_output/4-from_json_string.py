d97ba261f61446f5ce4a31e4113213b2a115eb17
treserio <treserio@hotmail.com>
2021-10-15 20:05:41 -0500

    4.1 fixed func def

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -3,6 +3,6 @@
 import json
 
 
-def to_json_string(my_obj):
+def from_json_string(my_str):
     '''Return an object based on JSON string'''
-    return json.loads(my_obj)
+    return json.loads(my_str)
