ab5118bd2e8920096663ffd154d2d51c3a3c7759
treserio <treserio@hotmail.com>
2021-10-18 11:12:50 -0500

    0 init

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,10 +2,16 @@
 '''base init method'''
 
 
-def __init__(self, id=None):
-    self.__nb_objects = 0
-    if id != None:
-        self.id = id
-    else:
-        self.__nb_objects += 1
-        self.id = self.__nb_objects
+class Base:
+    '''Base class for geometry classes
+    '''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''Iniitialization Method'''
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
