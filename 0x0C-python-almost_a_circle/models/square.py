821f0a27808dcb5558360e9fddf8510f70daff01
treserio <treserio@hotmail.com>
2021-10-21 20:13:19 -0500

    square, rm size valid

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,6 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         '''init method for square'''
-        self.verify_WH("size", size)
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
