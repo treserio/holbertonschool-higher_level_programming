4a6b03252c552cbe717fad9e0ec5cec330d775d9
Tres Serio <treserio@hotmail.com>
2021-11-03 22:50:52 -0500

    5-9 init

---
+++ 0x01-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- Creates a table
+CREATE TABLE IF NOT EXISTS unique_id(id INT NOT NULL DEFAULT 1, name VARCHAR(256), UNIQUE INDEX(id));
---
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- Lists cities in california
+SELECT id, name FROM cities WHERE state_id IN (
+	SELECT id FROM states WHERE name LIKE '%California%'
+)
+ORDER BY id ASC;
