d00c61b23dec914fbd77f0f5a5bc873c65a0db38
Tres Serio <treserio@hotmail.com>
2021-11-03 23:07:13 -0500

    10-16 init

---
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- shows by genre_id
+SELECT tv_genres.name genre, COUNT(tv_show_genres.genre_id) number_of_shows
+	FROM tv_genres
+		INNER JOIN tv_show_genres
+			ON tv_genres.id = tv_show_genres.genre_id
+	GROUP BY tv_genres.name, tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
