241f3897349b59a5beb07668aab9ba948a6857af
Tres Serio <treserio@hotmail.com>
2021-11-17 15:48:09 -0600

    1.3 +passwd

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     try:
         db = MySQLdb.connect(host='localhost',
                              user=argv[1],
+                             passwd=argv[2],
                              db=argv[3],
                              port=3306
                              )
