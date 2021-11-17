6aa4e465952c73528c3cd98aa3125993e82a9b67
Tres Serio <treserio@hotmail.com>
2021-11-17 14:15:08 -0600

    0 init

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+from MySQLdb import connections
+
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             user=argv[1],
+                             passwd=argv[2],
+                             db=argv[3],
+                             port=3306
+                             )
+    except Exception:
+        exit()
+
+    curse = db.cursor()
+    curse.execute("SELECT * FROM states")
+
+    print("\n".join(str(rec) for rec in curse.fetchall()))
+
+    curse.close()
+    db.close()
