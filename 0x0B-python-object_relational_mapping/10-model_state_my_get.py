bbf53287fc86af8240a6e5579ef01b8619de9f41
Tres Serio <treserio@hotmail.com>
2021-11-17 22:29:13 -0600

    10 init

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+'''List first states using SQLalchemy'''
+
+
+if __name__ == '__main__':
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    Eng = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                        .format(argv[1], argv[2], argv[3]),
+                        pool_pre_ping=True)
+    Base.metadata.create_all(Eng)
+    Sess = sessionmaker(bind=Eng)
+
+    try:
+        print(Sess().query(State)
+              .order_by(State.id)
+              .filter(State.name == argv[4])[0].id
+              )
+    except Exception:
+        print('Not Found')
+
+    Sess().close
