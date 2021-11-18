d79fd39a97d1cc738f4bc72b29c735cf5509ca97
Tres Serio <treserio@hotmail.com>
2021-11-17 19:25:55 -0600

    4.1 fixed pw

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     try:
         db = MySQLdb.connect(host='localhost',
                              user=argv[1],
-                             passwd='',
+                             passwd=argv[2],
                              db=argv[3],
                              port=3306
                              )
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         exit()
 
     curse = db.cursor()
-    print('running')
+
     curse.execute(
         "SELECT cities.id, cities.name, states.name\
         FROM cities JOIN states ON cities.state_id=states.id\
