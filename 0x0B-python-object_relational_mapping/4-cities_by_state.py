738f0a2592d4869b938701e31fe5dc327bca1cc2
Tres Serio <treserio@hotmail.com>
2021-11-17 19:22:56 -0600

    4 init

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+'''Print all states where name matches given str'''
+
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             user=argv[1],
+                             passwd='',
+                             db=argv[3],
+                             port=3306
+                             )
+    except Exception:
+        exit()
+
+    curse = db.cursor()
+    print('running')
+    curse.execute(
+        "SELECT cities.id, cities.name, states.name\
+        FROM cities JOIN states ON cities.state_id=states.id\
+        ORDER BY id")
+
+    for rec in curse.fetchall():
+        print(rec)
+
+    curse.close()
+    db.close()
