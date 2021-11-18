fcbf178074fddc18f813e6f6472f3a1d1fa6cb35
Tres Serio <treserio@hotmail.com>
2021-11-17 19:04:58 -0600

    3 init

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+'''Print all states where name matches given str'''
+
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             user=argv[1],
+                             passwd=argv[2],
+                             db=argv[3],
+                             port=3306
+                             )
+    except Exception:
+        exit()
+
+    curse = db.cursor()
+    curse.execute(
+        "SELECT * FROM states\
+        WHERE name LIKE BINARY '{}'\
+        ORDER BY id".format(argv[4]))
+
+    for rec in curse.fetchall():
+        print(rec)
+
+    curse.close()
+    db.close()
