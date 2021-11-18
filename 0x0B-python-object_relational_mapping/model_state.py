f93feae7e9e3af50df4ad8a653dd0a0a7c295b00
Tres Serio <treserio@hotmail.com>
2021-11-17 20:13:47 -0600

    model_state another try at inherit

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -3,8 +3,10 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
-class State(declarative_base()):
+
+class State(Base):
     '''class for states table'''
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
