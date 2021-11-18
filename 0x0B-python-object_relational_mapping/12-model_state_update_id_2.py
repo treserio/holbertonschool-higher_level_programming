1c220aae62e537b27861aa57bd658f1dd446b5f2
Tres Serio <treserio@hotmail.com>
2021-11-17 23:15:49 -0600

    12 init

--- 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''List first states using SQLalchemy'''
+
+
+if __name__ == '__main__':
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    Eng = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                        .format(argv[1], argv[2], argv[3]),
+                        pool_pre_ping=True)
+
+    Sess = sessionmaker(bind=Eng)
+    s = Sess()
+    s.query(State).where(State.id == 2)[0].name = 'New Mexico'
+    s.commit()
+    s.close()
