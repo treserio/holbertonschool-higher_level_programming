4b12f02e92bbb1ec9510e0935b757a829316f0c9
Tres Serio <treserio@hotmail.com>
2021-11-17 18:49:53 -0600

    2 init

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+'''Print all states in order by id'''
+
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             user=argv[1],
+                             passwd=argv[2],
+                             db=argv[3],
+                             port=3306
+                             )
+    except Exception:
+        exit()
+
+    curse = db.cursor()
+    curse.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY " + argv[4] + " ORDER BY id ASC")
+
+    for rec in curse.fetchall():
+        print(rec)
+
+    curse.close()
+    db.close()
