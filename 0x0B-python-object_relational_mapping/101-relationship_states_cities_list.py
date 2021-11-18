3c9b4b68fc6ad4eca8555eb192f7a92c18e8cbcf
Tres Serio <treserio@hotmail.com>
2021-11-18 15:31:14 -0600

    101 init

--- 0x0B-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0B-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+'''List first states using SQLalchemy'''
+
+
+if __name__ == '__main__':
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from relationship_state import Base, State
+    from relationship_city import City
+
+    Eng = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                        .format(argv[1], argv[2], argv[3]),
+                        pool_pre_ping=True)
+    Base.metadata.create_all(Eng)
+    Sess = sessionmaker(bind=Eng)
+
+    for st in Sess().query(State):
+        print("{}: {}".format(st.id, st.name))
+        for ct in st.cities:
+            print("\t{}: {}".format(ct.id, ct.name))
+
+    Sess().close()
