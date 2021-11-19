1d3853ce556a7560d7334dc6028e1fa1067096b9
Tres Serio <treserio@hotmail.com>
2021-11-18 20:19:10 -0600

    101.4 better avg_temp

--- 0x00-SQL_introduction/101-avg_temperatures.sql
+++ 0x00-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- show average temperatures for cities
-SELECT city, AVG(value) AS agv_temp FROM temperatures GROUP BY city ORDER BY AVG(value) DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY AVG(value) DESC;
