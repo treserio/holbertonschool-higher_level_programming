1aa342e27ea7f7f48f084b83fc2d70a35028f12e
Tres Serio <treserio@hotmail.com>
2021-12-04 03:23:11 -0600

    100.1 sorted without datetime obj

--- 0x0C-python-network_1/100-github_commits.py
+++ 0x0C-python-network_1/100-github_commits.py
@@ -9,13 +9,24 @@ if __name__ == '__main__':
     repo = sys.argv[1]
     ownr = sys.argv[2]
 
+    def time_sort(L):
+        t_in = L['commit']['author']['date']
+        date = t_in.split('T')[0]
+        date_t = t_in.split('T')[1]
+
+        yr = date.split('-')[0]
+        mon = date.split('-')[1]
+        day = date.split('-')[2]
+
+        hr = date_t.split(':')[0]
+        mn = date_t.split(':')[1]
+
+        return yr, mon, day, hr, mn
+
     req = requests.get(
         'https://api.github.com/repos/{}/{}/commits'
         .format(ownr, repo)
     )
     jsonStr = req.json()
-
-    for i in range(10):
-        sha = jsonStr[i]['sha']
-        own = jsonStr[i]['commit']['author']['name']
-        print('{}: {}'.format(sha, own))
+    print("\n".join('{}: {}'.format(i['sha'], i['commit']['author']['name'])
+                    for i in sorted(jsonStr, key=time_sort)[:10]))
