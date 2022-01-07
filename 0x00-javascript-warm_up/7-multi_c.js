a3c6261853cb43eba94174d1a880cc4b648cd32d
Tres Serio <treserio@hotmail.com>
2022-01-06 23:11:35 -0600

    7.2 +missing msg

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]);
+const n = process.argv[2];
 
-for (i = 1; i; --i)
-  console.log("C is fun\n".repeat(num - 1) + "C is fun");
+if (parseInt(n) || n === '0') {
+  for (let i = n; i > 0; i--) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
