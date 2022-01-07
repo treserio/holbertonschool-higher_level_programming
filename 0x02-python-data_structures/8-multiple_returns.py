6a4a81201f0f67b3fab5cf0b9dc29c9f1e5fca15
Tres Serio <treserio@hotmail.com>
2022-01-07 11:08:35 -0600

    refactor #8

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence:
-        return len(sentence), sentence[0]
-    else:
-        return 0, None
+    return len(sentence), sentence[0] if sentence else None
