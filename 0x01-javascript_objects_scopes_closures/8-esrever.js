0e5b2a071e6d8ca70e9d9e9233bb4f17d061c9e2
Tres Serio <treserio@hotmail.com>
2022-01-07 20:50:10 -0600

    8.1 i>=0

--- 0x01-javascript_objects_scopes_closures/8-esrever.js
+++ 0x01-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const rez = [];
-  for (let i = list.length - 1; i > 0; --i) {
+  for (let i = list.length - 1; i >= 0; --i) {
     rez.push(list[i]);
   }
   return rez;
-}
+};
