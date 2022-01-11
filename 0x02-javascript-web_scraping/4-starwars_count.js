071b75ea30cfd530b02ab50eaaa34841007be5b5
Tres Serio <treserio@hotmail.com>
2022-01-11 16:41:08 -0600

    4.2 refactored to accept url

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const req = require('request');
-let cnt = 0;
-req.get('https://swapi-api.hbtn.io/api/films/', (e, r) => {
+req.get(process.argv[2], (e, r) => {
+  let cnt = 0;
   for (const movie of JSON.parse(r.body).results) {
     for (const char of movie.characters) {
-      if (char.split('/').slice(-2, -1)[0] === process.argv[2]) {
+      if (char.split('/').slice(-2, -1)[0] === '18') {
         ++cnt;
       }
     }
