52cbc620cfc19b08dbd15d98534db2a93e96cf1a
Tres Serio <treserio@hotmail.com>
2022-03-10 12:18:55 -0600

    3 shortened

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -8,8 +8,7 @@ if __name__ == '__main__':
 
     try:
         with request.urlopen(sys.argv[1]) as r:
-            html = r.read()
-            print(html.decode('UTF-8'))
+            print(r.read().decode('UTF-8'))
 
     except error.HTTPError as e:
         print('Error code: {}'.format(e.code))
